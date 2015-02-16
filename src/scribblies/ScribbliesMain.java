871fcda0fa1644908639626e9ce5ac58e387a188
dgli <facultyofmusic@gmail.com>
2015-02-16 00:32:15 -0500

    made default window and main thread

--- src/scribblies/ScribbliesMain.java
+++ src/scribblies/ScribbliesMain.java
@@ -3,5 +3,24 @@ package scribblies;
 /**
  * Created by dgli on 16/02/15.
  */
-public class ScribbliesMain {
+public class ScribbliesMain implements Runnable{
+
+    MainWindow mainWindow;
+
+    public static void main(String args[]){
+
+        Thread GUIThread = new Thread(new ScribbliesMain());
+        GUIThread.start();
+
+    }
+
+    @Override
+    public void run() {
+        mainWindow = new MainWindow();
+
+        mainWindow.setVisible(true);
+
+    }
+
+
 }
